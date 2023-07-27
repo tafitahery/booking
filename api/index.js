c8a0a27dbbc657199db662c6672014bd42cd49b6
Tafita Hery <tafitahery@gmail.com>
2023-07-27 17:27:55 +0300

    Installation & Node.js MongoDB Connection

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log('Connected to MongoDB.');
+  } catch (err) {
+    throw err;
+  }
+};
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected.');
+});
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected.');
+});
+
+app.listen(8800, () => {
+  connect();
+  console.log('Server is running on port 8800.');
+});
