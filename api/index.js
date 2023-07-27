ef57d1b1328abf5406735bf7259f10c75e719507
Tafita Hery <tafitahery@gmail.com>
2023-07-27 19:17:34 +0300

    Create Your First API using Node.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import authRoute from './routes/auth.js';
+import usersRoute from './routes/users.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
 
 const app = express();
 dotenv.config();
@@ -8,7 +12,6 @@ dotenv.config();
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-    console.log('Connected to MongoDB.');
   } catch (err) {
     throw err;
   }
@@ -20,6 +23,12 @@ mongoose.connection.on('disconnected', () => {
 mongoose.connection.on('connected', () => {
   console.log('MongoDB connected.');
 });
+
+// middlewares
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
 
 app.listen(8800, () => {
   connect();
