ccf4c12d6abf2b4e14411c44131009f565fd0207
Tafita Hery <tafitahery@gmail.com>
2023-07-28 15:43:00 +0300

    Node.js MongoDB Authentification (Login/Register)

---
+++ api/controllers/auth.js
@@ -0,0 +1,37 @@
+import User from '../models/User.js';
+import { createError } from '../utils/error.js';
+import bcrypt from 'bcryptjs';
+
+export const register = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hash });
+    await newUser.save();
+    res.status(200).send('User has been created.');
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return next(createError(404, 'User not found'));
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect) {
+      return next(createError(400, 'Wrong password or usename'));
+    }
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+    res.status(200).json({ ...otherDetails });
+  } catch (err) {
+    next(err);
+  }
+};
