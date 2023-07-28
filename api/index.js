49fa5d15df7eaf8e5ba40b5076e4b4feeb5cd108
Tafita Hery <tafitahery@gmail.com>
2023-07-28 14:11:35 +0300

    Node.js MongoDB CRUD Operations

--- api/index.js
+++ api/index.js
@@ -25,6 +25,7 @@ mongoose.connection.on('connected', () => {
 });
 
 // middlewares
+app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/hotels', hotelsRoute);
