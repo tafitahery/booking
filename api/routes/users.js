dc138d6c7faa35a293e719ab6517d8a28faee57f
Tafita Hery <tafitahery@gmail.com>
2023-07-30 10:28:19 +0300

    Node.js JWT Role-Based Athorization Verification

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,12 +5,25 @@ import {
   getUsers,
   updateUser,
 } from '../controllers/user.js';
+import { verifyAdmin, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
-router.get('/:id', getUser);
-router.get('/', getUsers);
+// router.get('/authentification', verifyToken, (req, res, next) => {
+//   res.send('Hello user, you are logged in.');
+// });
+
+// router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+//   res.send('Hello user, you are logged in and you can delete your account.');
+// });
+
+// router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+//   res.send('Hello admin, you are logged in and you can delete all account.');
+// });
+
+router.put('/:id', verifyUser, updateUser);
+router.delete('/:id', verifyUser, deleteUser);
+router.get('/:id', verifyAdmin, getUser);
+router.get('/', verifyAdmin, getUsers);
 
 export default router;
